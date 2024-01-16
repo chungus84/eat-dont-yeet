950bc5e7debb81b0adb88319a155ba7b41db1f82
kenichi beveridge <kenbev84@gmail.com>
2024-01-16 21:17:59 +0000

    Added User entit

---
+++ auth-backend/src/main/java/com/eatdontyeet/authbackend/entity/User.java
@@ -0,0 +1,15 @@
+package com.eatdontyeet.authbackend.entity;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    private LocalDate createdTa;
+
+}
