54bdff71156630d854ae0bb69a52179482ba5538
kenichi beveridge <kenbev84@gmail.com>
2024-01-17 16:38:49 +0000

    Added Test to throw persistenceException when first name is too long

---
+++ auth-backend/src/test/java/com/eatdontyeet/authbackend/entity/UserIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.eatdontyeet.authbackend.entity;
+
+import jakarta.persistence.PersistenceException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserIntegrationTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = new User("Test", "Testerson", "testtest", "test@test.com", "passtest");
+    }
+
+    @DisplayName("Create and return a user entity")
+    @Test
+    void testUserEntity_WhenValidUserDetailsAreProvided_ShouldReturnStoredUserDetails() {
+
+        // Act
+        User storedUser = testEntityManager.persistAndFlush(user);
+
+        // Assert
+        assertTrue(storedUser.getId() > 0, "Id should have been greater than 0");
+        assertEquals(user.getUserId(), storedUser.getUserId(), "Returned UserId was not as expected");
+        assertEquals(user.getFirstName(), storedUser.getFirstName(), "Returned First Name was not as expected");
+        assertEquals(user.getLastName(), storedUser.getLastName(), "Returned Last Name was not as expected");
+        assertEquals(user.getUserName(), storedUser.getUserName(), "Returned UserName was not as expected");
+        assertEquals(user.getEmail(), storedUser.getEmail(), "Returned Email was not as expected");
+        assertEquals(user.getCreatedAt(), storedUser.getCreatedAt(), "Returned a CreatedAt date that was not as expected");
+
+    }
+
+    @DisplayName("Should throw PersistenceException when firstName is too long")
+    @Test
+    void testUserEntity_WhenGivenAFirstNameThatIsTooLong_ShouldThrowPersistenceException() {
+
+        // Arrange
+        user.setFirstName("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+        // Act
+        assertThrows(PersistenceException.class, ()-> {
+            testEntityManager.persistAndFlush(user);
+        }, "Should have thrown PersistenceException");
+
+    }
+
+
+}
