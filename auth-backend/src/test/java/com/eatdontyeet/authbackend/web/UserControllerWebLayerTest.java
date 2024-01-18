4f4da33bd8f723208321012c09cba4f411dcd12d
kenichi beveridge <kenbev84@gmail.com>
2024-01-18 11:55:56 +0000

    Added WebLayer Test for UserController

---
+++ auth-backend/src/test/java/com/eatdontyeet/authbackend/web/UserControllerWebLayerTest.java
@@ -0,0 +1,63 @@
+package com.eatdontyeet.authbackend.web;
+
+import com.eatdontyeet.authbackend.entity.User;
+import com.eatdontyeet.authbackend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class })
+public class UserControllerWebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private User user;
+
+
+    @MockBean
+    UserService userService;
+
+    @BeforeEach
+    void init() {
+        user = new User("Test", "Testerson", "testtest", "test@test.com", "passtest");
+    }
+
+    @DisplayName("User can be created")
+    @Test
+    void testSaveUser_WhenGivenValidDetails_ShouldReturnCreatedUserDetails() throws Exception {
+
+        // Arrange
+        when(userService.saveUser(any(User.class))).thenReturn(user);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(user));
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse headers = mvcResult.getResponse();
+
+        // Assert
+        assertEquals(user.getUserName(), headers.getHeader("userName"), "The returned UserName is incorrect");
+        assertEquals(user.getUserId(), headers.getHeader("userId"), "The returned userId is incorrect");
+
+    }
+}
