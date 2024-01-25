e4516d172725377dbd0986696e17e951ebfaf82c
kenichi beveridge <kenbev84@gmail.com>
2024-01-25 19:20:57 +0000

    Added header and started front end

---
+++ frontend-js/src/Componenets/utils/Header.jsx
@@ -0,0 +1,15 @@
+import './css/Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <div className="container nav-container">
+                <div className="navbar nav-header">
+                    <h2>eat<span>DON'T</span>yeet</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
